3011ea051163d7cb9a64bdd9442ab5c8a3d3042e
Rey-24-rey <h.kashazhum@gmail.com>
2025-02-19 14:04:33 +0300

    feat:port environment

--- app.py
+++ app.py
@@ -125,4 +125,6 @@ def process_excel(filepath):
     return jsonify(convert_numpy(response_data))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
